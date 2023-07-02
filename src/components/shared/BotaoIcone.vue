1fc81e1c8d22af13401537654b0320aaed0e6f61
joaoemc2 <joaofr.gg@gmail.com>
2023-07-02 00:57:20 -0300

    :art: novo componente

---
+++ src/components/shared/BotaoIcone.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const prop = defineProps<{
+  texto: string
+  icone: string
+}>()
+</script>
+
+<template>
+  <button class="btn-container">
+    <i :class="`bi bi-${prop.icone}`"></i>
+    <p class="btn-texto">{{ prop.texto }}</p>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+@import '@/styles/variables.scss';
+.btn-container {
+  border: none;
+  background: none;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  & :hover {
+    opacity: $hover;
+  }
+
+  .btn-texto {
+    color: $cor-amarela;
+    font-size: 16px;
+    font-weight: $peso-bold;
+  }
+  i {
+    font-size: 38px;
+    color: $cor-amarela;
+  }
+}
+</style>
