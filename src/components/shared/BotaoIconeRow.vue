9a1f70d2f620ddec37fb3dbf431250b35d6f4a21
joaoemc2 <joaofr.gg@gmail.com>
2023-07-02 04:25:26 -0300

    :lipstick: novo tipo de botão

---
+++ src/components/shared/BotaoIconeRow.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const prop = defineProps<{
+  texto: string
+  icone: string
+}>()
+</script>
+
+<template>
+  <button class="btn-container">
+    <i :class="`bi bi-${prop.icone}`"></i>
+    <p class="btn-texto">{{ prop.texto }}</p>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+@import '@/styles/variables.scss';
+.btn-container {
+  border: none;
+  background: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0px;
+  & :hover {
+    opacity: $hover;
+  }
+
+  .btn-texto {
+    color: $cor-amarela;
+    font-size: 16px;
+    font-weight: $peso-bold;
+  }
+  i {
+    font-size: 38px;
+    color: $cor-amarela;
+  }
+}
+</style>
