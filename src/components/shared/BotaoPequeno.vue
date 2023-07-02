60071be60f63501ac83b720e71136c09e66b5086
joaoemc2 <joaofr.gg@gmail.com>
2023-07-02 00:57:10 -0300

    :art: novo componente

---
+++ src/components/shared/BotaoPequeno.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const prop = defineProps<{
+  texto: string
+}>()
+</script>
+
+<template>
+  <button class="btn-container">
+    <p class="btn-texto">{{ prop.texto }}</p>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+@import '@/styles/variables.scss';
+.btn-container {
+  padding: 12px 32px;
+  border: none;
+  border-radius: 32px;
+  background-color: $cor-primaria;
+  .btn-texto {
+    color: $cor-branca;
+    font-size: $fonte-padrao;
+    font-weight: $peso-bold;
+  }
+}
+</style>
