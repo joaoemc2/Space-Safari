235e4199d65cd85a1ae728204866e18c2756fc5d
joaoemc2 <joaofr.gg@gmail.com>
2023-07-01 22:49:20 -0300

    :package: removido import desnecessário

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 </script>
 
